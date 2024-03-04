a6da976684bf336b7e5bec084a98e44681c053b9
ElectronicAPU <apurbanaskar3206@gmail.com>
2024-03-04 12:43:46 +0530

    sign in and sign up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <>
-      <main>
+      <main className="min-h-screen h-full w-full">
         <Outlet />
+        <ToastContainer />
       </main>
     </>
   );
