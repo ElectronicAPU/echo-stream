3e1f2506058e4ad516903ce553d1078eb4d5a10d
ElectronicAPU <apurbanaskar3206@gmail.com>
2024-03-05 11:53:58 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -53,12 +53,12 @@ const Footer = () => {
                 <img src="/images/logo.png" className="w-8" />
                 <h1 className="font-bold text-xl text-white">ECHO STREAM</h1>
               </div>
-              <div className="">
+              <div className=" ">
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10422.789638214517!2d88.3364315863532!3d22.541200153665137!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a02771346ae015d%3A0xb540e4bce39763!2sVictoria%20Memorial!5e0!3m2!1sen!2sin!4v1709577436511!5m2!1sen!2sin"
                   width="400"
                   height="300"
-                  style={{ border: 0, borderRadius: "10px" }}
+                  style={{ border: 0, borderRadius: "10px", width: "100%" }}
                   allowFullScreen=""
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
